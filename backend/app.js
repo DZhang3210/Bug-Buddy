90e5d44475f62c3720eb345012ada19d25129ee0
David Zhang <53838139+DZhang3210@users.noreply.github.com>
2024-01-11 11:50:36 -0500

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+
+const punycode = require('punycode/');
+const express = require('express')
+const mongoose = require('mongoose')
+const issueRoutes = require('./routes/issues')
+const signupRoutes = require('./routes/user')
+const commentRoutes = require('./routes/comment')
+const resolvedIssuesRoutes = require('./routes/rIssues')
+
+//express app
+const corsOptions = {
+    origin: "http://localhost:3000" // frontend URI (ReactJS)
+}
+const app = express();
+const cors = require('cors')
+app.use(express.json());
+app.use(cors(corsOptions))
+
+//Routes
+app.use('/api/comment', commentRoutes)
+
+//app.use('/api/rissue, resolvedIssueRoutes')
+
+app.use('/api/issue',issueRoutes)
+
+app.use('/api/user', signupRoutes)
+
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(
+    //Start up express app
+    app.listen(process.env.PORT || 4000, () => {
+        console.log('connected to db & listening on port '+ process.env.PORT)
+    })
+)
+.catch(err => console.log(err))
+
+//Developement function, checking requests
+app.use((req, res, next)=>{
+    console.log(req.path, req.method)
+    next();
+})
+
+//Default Home app
+app.get('/', (req, res) => {
+    res.json({mssg: "Welcome to the app"})
+})
+
+//const URL =   "http://localhost:4000"
+
+
+
